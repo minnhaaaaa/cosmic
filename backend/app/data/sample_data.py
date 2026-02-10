ca43de951af9e69dbaef58c781d24c6305aff3e2
Minha <179120250+minnhaaaaa@users.noreply.github.com>
2026-02-10 08:53:48 +0530

    Fix classification bug

--- backend/app/data/sample_data.py
+++ backend/app/data/sample_data.py
@@ -4,28 +4,48 @@ SAMPLE_TRAINING = [
     ("My invoice shows an incorrect amount", "Billing"),
     ("Need help understanding my bill", "Billing"),
     ("Please update my billing info", "Billing"),
+    ("Why was I charged for this?", "Billing"),
+    ("There is a duplicate charge on my account", "Billing"),
+    ("Can you explain these charges?", "Billing"),
+    
     # Technical
     ("App crashes when I try to upload a file", "Technical"),
     ("Unable to login due to server error", "Technical"),
     ("Getting 500 error on checkout", "Technical"),
     ("My internet is not working", "Technical"),
+    ("The website is down", "Technical"),
+    ("Getting an error when trying to access the site", "Technical"),
+    ("App keeps freezing", "Technical"),
+    
     # Account
     ("I want to change my account email", "Account"),
     ("How do I reset my password?", "Account"),
     ("Please help me update my profile details", "Account"),
     ("I cannot login to my account", "Account"),
-
+    ("My password reset is not working", "Account"),
+    ("I need to update my account information", "Account"),
+    ("How do I change my email address?", "Account"),
+    ("I forgot my password", "Account"),
+    
     # Feature
     ("Can you add dark mode to the app?", "Feature"),
     ("Request: export my data as CSV", "Feature"),
     ("It would be great to have multi-language support", "Feature"),
-
+    ("Can you add a mobile app?", "Feature"),
+    ("I would like to see a search feature", "Feature"),
+    
     # Refund Request
     ("I'd like a refund for my last purchase", "Refund Request"),
     ("Request refund for order #12345", "Refund Request"),
     ("I was billed but didn't receive the service, please refund", "Refund Request"),
+    ("I want my money back", "Refund Request"),
+    ("Please refund my payment", "Refund Request"),
+    ("Can I get a refund for this order?", "Refund Request"),
+    
     # Service Complaint
     ("Support ticket unresolved for days", "Service Complaint"),
     ("Very poor customer service experience", "Service Complaint"),
     ("Unhappy with the service quality", "Service Complaint"),
+    ("Your support team was rude", "Service Complaint"),
+    ("The service is terrible", "Service Complaint"),
 ]
